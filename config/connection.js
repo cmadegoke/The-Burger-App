0c4bd57ab1004a926a89fd9434902827a3d96015
Catherine Adegoke <cmadegoke@gmail.com>
2020-03-07 09:59:42 -0500

    updated

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 
-const connection = process.env.JAWSDB_URL
-?mysql.createconnection(process.env.JAWSDB_URL)
-:mysql.createConnection({
+const connection = process.env.JAWSDB_URL ?
+mysql.createConnection(process.env.JAWSDB_URL):
+ mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: process.env.DB_USER,
